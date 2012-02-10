b67d1811d3e0234953d30c390ad6233e61c95e57
cedricbonhomme <devnull@localhost>
2012-02-10 11:13:39 +0100

    Updated an example file.

--- source/examples/example-1.sh
+++ source/examples/example-1.sh
@@ -5,6 +5,6 @@
 
 mkdir captures data
 sudo tcpdump -p -i eth0 -s 0 -w captures/snif.pcap
-python pcap_to_object1.py -i captures/snif.pcap -o data/dic.pyobj
+python pcap_to_object.py -i captures/snif.pcap -o data/dic.pyobj
 python object_to_graphviz.py -i ./data/dic.pyobj
 dot -Tpng -o ./data/graphviz.png ./data/ip.dot
