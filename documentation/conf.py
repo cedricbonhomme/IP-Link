8119dd6d588cc81a72615275ab923ffc16373309
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-01-11 07:31:02 +0100

    Switch theme to alabaster.

--- documentation/conf.py
+++ documentation/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'IP-Link'
-copyright = u'2009-2015, Cédric Bonhomme, Jérôme Hussenet'
+copyright = u'2009-2016, Cédric Bonhomme, Jérôme Hussenet'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
