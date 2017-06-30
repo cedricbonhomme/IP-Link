650f1deca41244239e652107feaa4733538e7059
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-07-01 00:23:07 +0200

    mock out the imports for the module with a C extension (pcap) for ReadTheDocs

--- documentation/conf.py
+++ documentation/conf.py
@@ -13,6 +13,20 @@
 
 import sys, os
 
+
+# from: https://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+try:
+    from unittest.mock import MagicMock
+except:
+    from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+MOCK_MODULES = ['pcap']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
