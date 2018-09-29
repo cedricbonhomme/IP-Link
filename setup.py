cb04c0236013e041be1acf8b496a9a1d9468c448
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-09-30 00:43:01 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = [
+    'source'
+]
+
+scripts = []
+
+requires = ['pypacker', 'networkx']
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+with codecs.open(os.path.join(here, 'NEWS.rst'), encoding='utf-8') as f:
+    changelog = f.read()
+
+setup(
+    name='IP-Link',
+    version='0.2',
+    author='Cédric Bonhomme',
+    author_email='cedric@cedricbonhomme.org',
+    packages=packages,
+    include_package_data=True,
+    scripts=scripts,
+    url='https://github.com/cedricbonhomme/Stegano',
+    description='',
+    long_description=readme + '\n|\n\n' + changelog,
+    platforms = ['Linux'],
+    license='GPLv3',
+    install_requires=requires,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Security',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+    ]
+)
